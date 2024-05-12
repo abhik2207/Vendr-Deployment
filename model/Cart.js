441ee7e7f9cbe5426d0f3dc9b18082f4cfc1d8c2
Abhik <abhikgupta01@gmail.com>
2024-05-12 16:10:06 +0530

    Added Colors, Sizes and Highlights to products

--- model/Cart.js
+++ model/Cart.js
@@ -4,7 +4,9 @@ const { Schema } = mongoose;
 const cartSchema = new Schema({
     product: { type: Schema.Types.ObjectId, ref: 'Product', required: [true, 'Please provide a product ID'] },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'Please provide an user ID'] },
-    quantity: {type: Number, required: [true, 'Please provide quantity of the product']},
+    quantity: { type: Number, required: [true, 'Please provide quantity of the product'] },
+    color: { type: Schema.Types.Mixed },
+    size: { type: Schema.Types.Mixed }
 });
 
 const virtual = cartSchema.virtual('id');
--- model/Product.js
+++ model/Product.js
@@ -7,11 +7,14 @@ const productSchema = new Schema({
     category: { type: String, required: [true, 'Please provide a category for the product'] },
     description: { type: String, required: [true, 'Please provide a description for the product'] },
     price: { type: Number, min: [0, 'Price of product cannot be less than 0'], required: [true, 'Please provide a price for the product'], max: [100000, 'Price of product cannot be greater than 100,000'] },
-    discountPercentage: { type: Number, min: [0, 'Discount percentage of product cannot be less than 0'],  max: [90, 'Discount percentage of product cannot be gearter than 90'] },
+    discountPercentage: { type: Number, min: [0, 'Discount percentage of product cannot be less than 0'], max: [90, 'Discount percentage of product cannot be gearter than 90'] },
     rating: { type: Number, min: [0, 'Rating of product cannot be less than 0'], max: [5, 'Rating of product cannot be greater than 5'], default: 0 },
     stock: { type: Number, min: [0, 'Stock of product cannot be less than 0'], default: 0 },
     thumbnail: { type: String, required: [true, 'Provide a thumbnail of the product'] },
     images: [String],
+    colors: { type: [Schema.Types.Mixed] },
+    sizes: { type: [Schema.Types.Mixed] },
+    highlights: { type: [String] },
     deleted: { type: Boolean, default: false }
 });
 
